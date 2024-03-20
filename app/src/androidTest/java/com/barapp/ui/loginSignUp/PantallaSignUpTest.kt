87a36f6ad9e06dad67bae48f4dd4f8536fe4d414
ChortJulio <chortjulio@gmail.com>
2024-03-20 18:11:50 -0300

    BA-124 Add SignUp testing and update gradle
    Ahora la pantalla de sign up contiene testing, el gradle se actualizó para que utilice fragment-testing y se eliminó un testing deprecado.

---
+++ app/src/androidTest/java/com/barapp/ui/loginSignUp/PantallaSignUpTest.kt
@@ -0,0 +1,220 @@
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.GrantPermissionRule
+import com.barapp.R
+import com.barapp.ui.MainActivity
+import com.google.android.material.textfield.TextInputLayout
+import org.hamcrest.Description
+import org.hamcrest.Matchers
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import androidx.test.platform.app.InstrumentationRegistry
+
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class PantallaSignUpTest {
+
+  // Change the email each time you run the test
+  private val email = "test3@test.com"
+
+  @Rule
+  @JvmField
+  var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+  @Rule
+  @JvmField
+  var mGrantPermissionRule =
+    GrantPermissionRule.grant(
+      "android.permission.ACCESS_FINE_LOCATION"
+    )
+
+  @Test
+  fun validSignUpProcess() {
+    // Press register button
+    val registerButton = onView(withId(R.id.botonRegistrarse))
+    registerButton.perform(click())
+
+    // Enter valid name
+    val nameInput = onView(
+      Matchers.allOf(
+        isDescendantOfA(withId(R.id.txtViewNombre)), isAssignableFrom(
+          EditText::class.java
+        )
+      )
+    )
+    nameInput.perform(replaceText("Test"), closeSoftKeyboard())
+
+//    // Enter valid last name
+    val lastNameInput = onView(
+      Matchers.allOf(
+        isDescendantOfA(withId(R.id.txtViewApellido)), isAssignableFrom(
+          EditText::class.java
+        )
+      )
+    )
+    lastNameInput.perform(replaceText("User"), closeSoftKeyboard())
+
+    // Enter valid email
+
+    val emailInput = onView(
+      Matchers.allOf(
+        isDescendantOfA(withId(R.id.txtViewEmail)), isAssignableFrom(
+          EditText::class.java
+        )
+      )
+    )
+    emailInput.perform(replaceText(email), closeSoftKeyboard())
+
+    // Enter valid phone number
+
+    val phoneInput = onView(
+      Matchers.allOf(
+        isDescendantOfA(withId(R.id.txtViewTelefono)), isAssignableFrom(
+          EditText::class.java
+        )
+      )
+    )
+    phoneInput.perform(replaceText("1234567890"), closeSoftKeyboard())
+
+    // Enter valid password
+    val passwordInput = onView(
+      Matchers.allOf(
+        isDescendantOfA(withId(R.id.txtViewContrasenia)), isAssignableFrom(
+          EditText::class.java
+        )
+      )
+    )
+    passwordInput.perform(replaceText("password123"), closeSoftKeyboard())
+
+    // Confirm password
+    val confirmPasswordInput = onView(
+      Matchers.allOf(
+        isDescendantOfA(withId(R.id.txtViewConfirmeContrasenia)), isAssignableFrom(
+          EditText::class.java
+        )
+      )
+    )
+    confirmPasswordInput.perform(replaceText("password123"), closeSoftKeyboard())
+
+    // Click on create account button
+//    onView(withId(R.id.botonCrearCuentaSignup)).perform(click())
+    val createAccountButton = onView(withId(R.id.botonCrearCuentaSignup))
+    createAccountButton.perform(click())
+
+    // Wait a bit
+    Thread.sleep(2000)
+
+    // Check if login was successful
+    val labelDestacados = onView(
+      Matchers.allOf(
+        isAssignableFrom(TextView::class.java),
+        withId(R.id.labelDestacados),
+        isDisplayed()
+      )
+    )
+    labelDestacados.check(matches(isDisplayed()))
+  }
+
+  @Test
+  fun signUpWithInvalidEmail() {
+//    val scenario = launchFragmentInContainer<PantallaSignUp>(themeResId = R.style.Theme_BarApp)
+    // Press register button
+    val registerButton = onView(withId(R.id.botonRegistrarse))
+    registerButton.perform(click())
+
+    // Enter valid name
+    val nameInput = onView(
+      Matchers.allOf(
+        isDescendantOfA(withId(R.id.txtViewNombre)), isAssignableFrom(
+          EditText::class.java
+        )
+      )
+    )
+    nameInput.perform(replaceText("Test"), closeSoftKeyboard())
+
+    // Enter valid last name
+    val lastNameInput = onView(
+      Matchers.allOf(
+        isDescendantOfA(withId(R.id.txtViewApellido)), isAssignableFrom(
+          EditText::class.java
+        )
+      )
+    )
+    lastNameInput.perform(replaceText("User"), closeSoftKeyboard())
+
+    // Enter invalid email
+    val emailInput = onView(
+      Matchers.allOf(
+        isDescendantOfA(withId(R.id.txtViewEmail)), isAssignableFrom(
+          EditText::class.java
+        )
+      )
+    )
+    emailInput.perform(replaceText("invalidEmail"), closeSoftKeyboard())
+
+    // Enter valid phone number
+    val phoneInput = onView(
+      Matchers.allOf(
+        isDescendantOfA(withId(R.id.txtViewTelefono)), isAssignableFrom(
+          EditText::class.java
+        )
+      )
+    )
+    phoneInput.perform(replaceText("1234567890"), closeSoftKeyboard())
+
+    // Enter valid password
+    val passwordInput = onView(
+      Matchers.allOf(
+        isDescendantOfA(withId(R.id.txtViewContrasenia)), isAssignableFrom(
+          EditText::class.java
+        )
+      )
+    )
+    passwordInput.perform(replaceText("password123"), closeSoftKeyboard())
+
+    // Confirm password
+    val confirmPasswordInput = onView(
+      Matchers.allOf(
+        isDescendantOfA(withId(R.id.txtViewConfirmeContrasenia)), isAssignableFrom(
+          EditText::class.java
+        )
+      )
+    )
+    confirmPasswordInput.perform(replaceText("password123"), closeSoftKeyboard())
+
+    // Click on create account button
+    val createAccountButton = onView(withId(R.id.botonCrearCuentaSignup))
+    createAccountButton.perform(click())
+
+    // Check if sign up was unsuccessful by checking if the user is still on the sign up screen
+    val context = InstrumentationRegistry.getInstrumentation().targetContext
+    val expectedErrorText = context.getString(R.string.error_mail_no_valido)
+    onView(withId(R.id.txtViewEmail)).check(matches(hasTextInputLayoutErrorText(expectedErrorText)))
+  }
+
+  private fun hasTextInputLayoutErrorText(expectedErrorText: String): TypeSafeMatcher<View?> {
+    return object : TypeSafeMatcher<View?>() {
+      override fun describeTo(description: Description?) {}
+      override fun matchesSafely(view: View?): Boolean {
+        if (view !is TextInputLayout) {
+          return false
+        }
+        val error = (view as TextInputLayout).error
+          ?: return false
+        val hint = error.toString()
+        return expectedErrorText == hint
+      }
+    }
+  }
+}
