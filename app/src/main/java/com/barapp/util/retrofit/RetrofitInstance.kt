e06ccb19743a946046e51ba2da5849818b3f8b02
ChortJulio <chortjulio@gmail.com>
2024-05-29 11:04:11 -0300

    Delete comments and legacy code

--- app/src/main/java/com/barapp/util/retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/barapp/util/retrofit/RetrofitInstance.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-  private const val BASE_URL = "http://172.21.24.173:8080/"
+  private const val BASE_URL = "http://172.28.175.28:8080/"
 
   private lateinit var context: Context
 
@@ -19,58 +19,19 @@ object RetrofitInstance {
     .addInterceptor(Interceptor { chain ->
       val original = chain.request()
 
-      // Retrieve the JSESSIONID from SharedPreferences
       val sharedPref = context.getSharedPreferences("MyApp", Context.MODE_PRIVATE)
       val jsessionId = sharedPref.getString("JSESSIONID", "")
 
       println("JSESSIONID: $jsessionId")
 
-      // Customize the request
       val request = original.newBuilder()
         .header("Cookie", "JSESSIONID=$jsessionId")
         .method(original.method(), original.body())
         .build()
 
-      // Customize or return the response
       chain.proceed(request)
     })
     .build()
-
-//  val loginService: LoginService by lazy {
-//    Retrofit.Builder()
-//      .baseUrl(BASE_URL)
-//      .client(client)
-//      .addConverterFactory(GsonConverterFactory.create())
-//      .build()
-//      .create(LoginService::class.java)
-//  }
-//
-//  val restaurantApiService: RestaurantApiService by lazy {
-//    Retrofit.Builder()
-//      .baseUrl(BASE_URL)
-//      .client(client)
-//      .addConverterFactory(GsonConverterFactory.create())
-//      .build()
-//      .create(RestaurantApiService::class.java)
-//  }
-//
-//  val userApiService: UserApiService by lazy {
-//    Retrofit.Builder()
-//      .baseUrl(BASE_URL)
-//      .client(client)
-//      .addConverterFactory(GsonConverterFactory.create())
-//      .build()
-//      .create(UserApiService::class.java)
-//  }
-//
-//  val reservationApiService: ReservationApiService by lazy {
-//    Retrofit.Builder()
-//      .baseUrl(BASE_URL)
-//      .client(client)
-//      .addConverterFactory(GsonConverterFactory.create())
-//      .build()
-//      .create(ReservationApiService::class.java)
-//  }
 
   private val retrofit: Retrofit by lazy {
     Retrofit.Builder()
