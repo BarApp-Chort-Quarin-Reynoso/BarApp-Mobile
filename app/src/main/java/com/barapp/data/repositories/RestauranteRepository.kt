53c1c7244a7e5f57ed1eb11af9768792c505d7eb
ChortJulio <chortjulio@gmail.com>
2024-06-08 12:12:38 -0300

    Update API: Get featured restaurants
    Falta actualizar la API para que acepte limit (creo que no lo hace)

--- app/src/main/java/com/barapp/data/repositories/RestauranteRepository.kt
+++ app/src/main/java/com/barapp/data/repositories/RestauranteRepository.kt
@@ -91,34 +91,52 @@ class RestauranteRepository private constructor() : IGenericRepository<Restauran
 
   fun buscarDestacados(callback: FirestoreCallback<List<Restaurante>>) {
     val listaRestaurantes: MutableList<Restaurante> = ArrayList()
-    db
-      .collection(COLECCION_RESTAURANTES)
-      .orderBy("puntuacion", Query.Direction.DESCENDING)
-      .limit(6)
-      .get()
-      .addOnCompleteListener { task ->
-        if (task.isSuccessful) {
-          for (documentSnapshot in task.result) {
-            val restauranteEntity = documentSnapshot.toObject(RestauranteEntity::class.java)
-            val ubicacionRestaurante = documentSnapshot.toObject(Ubicacion::class.java)
-            listaRestaurantes.add(fromEntity(restauranteEntity, ubicacionRestaurante, null))
-          }
-          callback.onSuccess(listaRestaurantes)
+    val queryParams = mapOf("puntuacion" to "desc", "limit" to "6")
+    api.getAllRestaurants(queryParams).enqueue(object : Callback<List<Restaurante>> {
+      override fun onResponse(call: Call<List<Restaurante>>, response: Response<List<Restaurante>>) {
+        if (response.isSuccessful) {
+          val data = response.body()
+          Timber.d("Data received: $data")
+          callback.onSuccess(data!!)
         } else {
-          Timber.w("Error recuperando Restaurantes")
-          callback.onError(task.exception!!)
+          Timber.e("Error: ${response.errorBody()}")
+          callback.onError(Throwable("Error recuperando Restaurantes"))
         }
       }
+
+      override fun onFailure(call: Call<List<Restaurante>>, t: Throwable) {
+        Timber.e(t)
+        callback.onError(t)
+      }
+    })
+//    db
+//      .collection(COLECCION_RESTAURANTES)
+//      .orderBy("puntuacion", Query.Direction.DESCENDING)
+//      .limit(6)
+//      .get()
+//      .addOnCompleteListener { task ->
+//        if (task.isSuccessful) {
+//          for (documentSnapshot in task.result) {
+//            val restauranteEntity = documentSnapshot.toObject(RestauranteEntity::class.java)
+//            val ubicacionRestaurante = documentSnapshot.toObject(Ubicacion::class.java)
+//            listaRestaurantes.add(fromEntity(restauranteEntity, ubicacionRestaurante, null))
+//          }
+//          callback.onSuccess(listaRestaurantes)
+//        } else {
+//          Timber.w("Error recuperando Restaurantes")
+//          callback.onError(task.exception!!)
+//        }
+//      }
   }
 
   override fun guardar(entidad: Restaurante) {
-    val restauranteUbicacionEntity = toRestauranteUbicacion(entidad)
-    db
-      .collection(COLECCION_RESTAURANTES)
-      .document(entidad.id)
-      .set(restauranteUbicacionEntity)
-      .addOnSuccessListener { Timber.d("Restaurante successfully written!") }
-      .addOnFailureListener { e -> Timber.w(e, "Error writing restaurante") }
+//    val restauranteUbicacionEntity = toRestauranteUbicacion(entidad)
+//    db
+//      .collection(COLECCION_RESTAURANTES)
+//      .document(entidad.id)
+//      .set(restauranteUbicacionEntity)
+//      .addOnSuccessListener { Timber.d("Restaurante successfully written!") }
+//      .addOnFailureListener { e -> Timber.w(e, "Error writing restaurante") }
   }
 
   override fun actualizar(entidad: Restaurante) {}
