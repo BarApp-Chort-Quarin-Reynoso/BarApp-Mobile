3870921f366395df2046b454c58052b024179694
FedericoQuarin <federicoquarin4@gmail.com>
2024-08-31 20:36:18 -0300

    fix: fix Crear Reserva UI problems

--- app/src/main/java/com/barapp/model/HorarioConCapacidadDisponible.kt
+++ app/src/main/java/com/barapp/model/HorarioConCapacidadDisponible.kt
@@ -1,9 +1,13 @@
 package com.barapp.model
 
-class HorarioConCapacidadDisponible (var horarios: List<String>, var tipoComida: TipoComida, var mesas: List<Mesa>){
-    constructor() : this(emptyList(), TipoComida.NINGUNO, emptyList())
+class HorarioConCapacidadDisponible(
+  var horarios: List<String>,
+  var tipoComida: TipoComida,
+  var mesas: List<Mesa>
+) {
+  constructor() : this(emptyList(), TipoComida.NINGUNO, emptyList())
 
-    override fun toString(): String {
-        return "HorarioConCapacidadDisponible{horarios=$horarios, tipoComida=$tipoComida, mesas=$mesas}"
-    }
+  override fun toString(): String {
+    return "HorarioConCapacidadDisponible{horarios=$horarios, tipoComida=$tipoComida, mesas=$mesas}"
+  }
 }
