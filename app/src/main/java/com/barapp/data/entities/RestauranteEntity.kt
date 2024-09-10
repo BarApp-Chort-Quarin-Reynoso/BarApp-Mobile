5b751dfa59065db5d9b8149e849f4ea06c2bde7e
ChortJulio <chortjulio@gmail.com>
2024-09-10 20:04:47 -0300

    Add cantidadOpiniones to RestauranteUsuario model and entity
    Se agregó la cantidad de opiniones tanto al modelo como a la entidad de Restauirante y RestauranteUsuario.

--- app/src/main/java/com/barapp/data/entities/RestauranteEntity.kt
+++ app/src/main/java/com/barapp/data/entities/RestauranteEntity.kt
@@ -7,14 +7,15 @@ class RestauranteEntity(
   var nombre: String,
   var estado: EstadoRestaurante,
   var puntuacion: Double,
+  var cantidadOpiniones: Int,
   var portada: String,
   var correo: String,
   var logo: String,
   var idDetalleRestaurante: String,
 ) {
-  constructor() : this("", EstadoRestaurante.ESPERANDO_HABILITACION, -1.0, "", "", "", "")
+  constructor() : this("", EstadoRestaurante.ESPERANDO_HABILITACION, -1.0, 0, "", "", "", "")
 
   override fun toString(): String {
-    return "RestauranteEntity(nombre='$nombre', estado='$estado', puntuacion=$puntuacion, portada='$portada', correo='$correo', logo='$logo', idDetalleRestaurante='$idDetalleRestaurante')"
+    return "RestauranteEntity(nombre='$nombre', estado='$estado', puntuacion=$puntuacion, cantidadOpiniones=$cantidadOpiniones, portada='$portada', correo='$correo', logo='$logo', idDetalleRestaurante='$idDetalleRestaurante')"
   }
 }
