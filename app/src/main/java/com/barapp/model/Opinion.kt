10871540a1cfac7eb60f86ba465ff9438742e430
ChortJulio <chortjulio@gmail.com>
2024-09-10 20:06:23 -0300

    Update Opinion model and mapper
    Se actualizó el modelo de la opinión para que coincida con el del proyecto web.

--- app/src/main/java/com/barapp/model/Opinion.kt
+++ app/src/main/java/com/barapp/model/Opinion.kt
@@ -2,28 +2,35 @@ package com.barapp.model
 
 import java.util.UUID
 
-class Opinion(id: String, var comentario: String, var nota: Double, var usuario: Usuario) :
+class Opinion(id: String, var comentario: String, var nota: Double, var usuario: Usuario, var horario: Horario, var fecha: String, var cantidadPersonas: Int) :
   BaseClass(id) {
-  constructor() : this(UUID.randomUUID().toString(), "undefined", -1.0, Usuario())
+  constructor() : this(UUID.randomUUID().toString(), "", -1.0, Usuario(), Horario(), "", -1)
 
   constructor(
     comentario: String,
     nota: Double,
     usuario: Usuario,
-  ) : this(UUID.randomUUID().toString(), comentario, nota, usuario)
+    horario: Horario,
+    fecha: String,
+    cantidadPersonas: Int
+  ) : this(UUID.randomUUID().toString(), comentario, nota, usuario, horario, fecha, cantidadPersonas)
 
   override fun toString(): String {
     return "Opinion{" +
       "id='" +
       id +
-      '\'' +
       ", comentario='" +
       comentario +
-      '\'' +
       ", nota=" +
       nota +
       ", usuario=" +
       usuario +
+      ", horario=" +
+      horario +
+      ", fecha='" +
+      fecha +
+      ", cantidadPersonas=" +
+      cantidadPersonas +
       '}'
   }
 }
